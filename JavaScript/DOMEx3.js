4b1566cbb3f81230563ab012b439cc232d48c32d
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-07-30 07:42:15 +0530

    practising flexbox and grid

---
+++ JavaScript/DOMEx3.js
@@ -0,0 +1,23 @@
+
+
+
+const containers = document.getElementsByClassName('btns');
+
+Array.from(containers).forEach((element) =>{
+    element.style.backgroundColor = 'lightblue';
+    element.style.padding = '5px';
+    element.style.border = '1px solid black';
+
+
+});
+
+const btns = document.getElementsByClassName('btn');
+
+Arrays.from(btns).forEach((element)=>{
+     button.style.padding = '10px 15px';
+    button.style.border = '1px solid darkred';
+    button.style.borderRadius = '4px';
+    button.style.margin = '5px';
+
+});
+
