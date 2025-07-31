df7578c6348e7557aed1b2982e01dcb1dded9458
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-07-31 10:29:58 +0530

    added time selection

--- Reminder/reminder.js
+++ Reminder/reminder.js
@@ -1,34 +1,49 @@
 const btn = document.getElementById("remindButton");
 const input = document.getElementById("textInput");
 const tasksList = document.getElementById("tasksList");
+const timeInput = document.getElementById("timeInput");
 
 btn.addEventListener("click", () => {
     const text = input.value.trim();
-    if (text === "") return;
+    const time = timeInput.value;
+    const timeSpan = document.createElement("span");
+    if (text === "" || time === "")
+    {
+    alert(" Please type the reminder and select time !");
+    }
 
-    const li = document.createElement("li");
+    else {
 
-    const span = document.createElement("span");
-    span.className = "task-text";
-    span.textContent = text;
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.className = "delete-btn";
+        timeSpan.className = "time-text";
+        timeSpan.textContent= time;
+        const li = document.createElement("li");
 
-    // Toggle 'done' on click
-    span.addEventListener("click", () => {
-        li.classList.toggle("done");
-    });
+        const span = document.createElement("span");
+        span.className = "task-text";
+        span.textContent = text;
 
-    // Delete reminder
-    deleteBtn.addEventListener("click", () => {
-        li.remove();
-    });
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.className = "delete-btn";
 
-    li.appendChild(span);
-    li.appendChild(deleteBtn);
-    tasksList.appendChild(li);
+        // Toggle 'done' on click
+        span.addEventListener("click", () => {
+            li.classList.toggle("done");
+        });
 
-    input.value = ""; // Clear input
+        // Delete reminder
+        deleteBtn.addEventListener("click", () => {
+            li.remove();
+        });
+
+
+        li.appendChild(span);
+        li.appendChild(timeSpan);
+        li.appendChild(deleteBtn);
+        // li.appendChild(timeInput.value());
+        tasksList.appendChild(li);
+
+        input.value = ""; // Clear input
+    }
 });
